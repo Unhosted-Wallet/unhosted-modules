bebf0cbb19de9d83c1aa91a0e833d99de2fd7fa2
zakrad <daniyal.rad@gmail.com>
2023-10-02 16:18:07 +0330

    🔨 add fallbakc handler storage

--- defi-strategies/contracts/handlers/BaseHandler.sol
+++ defi-strategies/contracts/handlers/BaseHandler.sol
@@ -8,18 +8,17 @@ import "./interface/IERC20Usdt.sol";
 abstract contract BaseHandler {
     using SafeERC20 for IERC20;
 
-    address public constant NATIVE_TOKEN_ADDRESS =
-        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public constant NATIVE_TOKEN_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    bytes32 internal constant FALLBACK_HANDLER_STORAGE_SLOT =
+        0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d4;
 
     function getContractName() public pure virtual returns (string memory);
 
-    function _tokenApprove(
-        address token,
-        address spender,
-        uint256 amount
-    ) internal {
+    function _tokenApprove(address token, address spender, uint256 amount) internal {
         /* solhint-disable no-empty-blocks */
-        try IERC20Usdt(token).approve(spender, amount) {} catch {
+        try IERC20Usdt(token).approve(spender, amount) {}
+        catch {
             IERC20(token).safeApprove(spender, 0);
             IERC20(token).safeApprove(spender, amount);
         }
@@ -27,16 +26,14 @@ abstract contract BaseHandler {
 
     function _tokenApproveZero(address token, address spender) internal {
         if (IERC20Usdt(token).allowance(address(this), spender) > 0) {
-            try IERC20Usdt(token).approve(spender, 0) {} catch {
+            try IERC20Usdt(token).approve(spender, 0) {}
+            catch {
                 IERC20Usdt(token).approve(spender, 1);
             }
         }
     }
 
-    function _getBalance(
-        address token,
-        uint256 amount
-    ) internal view returns (uint256) {
+    function _getBalance(address token, uint256 amount) internal view returns (uint256) {
         if (amount != type(uint256).max) {
             return amount;
         }
@@ -49,32 +46,15 @@ abstract contract BaseHandler {
         return IERC20(token).balanceOf(address(this));
     }
 
-    function _revertMsg(
-        string memory functionName,
-        string memory reason
-    ) internal pure {
-        revert(
-            string(
-                abi.encodePacked(
-                    getContractName(),
-                    "_",
-                    functionName,
-                    ": ",
-                    reason
-                )
-            )
-        );
+    function _revertMsg(string memory functionName, string memory reason) internal pure {
+        revert(string(abi.encodePacked(getContractName(), "_", functionName, ": ", reason)));
     }
 
     function _revertMsg(string memory functionName) internal pure {
         _revertMsg(functionName, "Unspecified");
     }
 
-    function _requireMsg(
-        bool condition,
-        string memory functionName,
-        string memory reason
-    ) internal pure {
+    function _requireMsg(bool condition, string memory functionName, string memory reason) internal pure {
         if (!condition) _revertMsg(functionName, reason);
     }
 }
