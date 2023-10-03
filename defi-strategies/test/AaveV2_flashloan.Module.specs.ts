768d466e03142493830dac06ef2d61a4c179d91a
zakrad <daniyal.rad@gmail.com>
2023-10-04 01:32:14 +0330

    ✅ add debt flashloan test

--- defi-strategies/test/AaveV2_flashloan.Module.specs.ts
+++ defi-strategies/test/AaveV2_flashloan.Module.specs.ts
@@ -7,12 +7,11 @@ import { makeEcdsaModuleUserOp } from "./utils/userOp";
 import {
   WRAPPED_NATIVE_TOKEN,
   AAVEPROTOCOL_V2_PROVIDER,
-  AWRAPPED_NATIVE_V2_TOKEN,
   DAI_TOKEN,
   ADAI_V2,
   AAVE_RATEMODE,
+  AWRAPPED_NATIVE_V2_DEBT_VARIABLE,
 } from "./utils/constants_eth";
-import { MAX_UINT256 } from "./utils/constants";
 
 import {
   getEntryPoint,
@@ -33,9 +32,8 @@ describe("AaveV2 flashloan", async () => {
   let strategyModule: Contract;
   let aaveV2handler: Contract;
   let WrappedETH: Contract;
-  let AWrappedETH: Contract;
+  let variableDebtWETH: Contract;
   let token: Contract;
-  let aToken: Contract;
   let provider: Contract;
   let lendingPool: Contract;
   let providerAddress: any;
@@ -69,21 +67,18 @@ describe("AaveV2 flashloan", async () => {
       await provider.getLendingPool()
     );
 
+    variableDebtWETH = await ethers.getContractAt(
+      "IVariableDebtToken",
+      AWRAPPED_NATIVE_V2_DEBT_VARIABLE
+    );
+
     WrappedETH = await (
       await ethers.getContractFactory("MockToken")
     ).attach(WRAPPED_NATIVE_TOKEN);
 
-    AWrappedETH = await (
-      await ethers.getContractFactory("MockToken")
-    ).attach(AWRAPPED_NATIVE_V2_TOKEN);
-
     token = await (
       await ethers.getContractFactory("MockToken")
     ).attach(DAI_TOKEN);
-
-    aToken = await (
-      await ethers.getContractFactory("MockToken")
-    ).attach(ADAI_V2);
 
     providerAddress = await getTokenProvider(token.address);
     providerAddress = await ethers.getSigner(providerAddress);
@@ -120,8 +115,11 @@ describe("AaveV2 flashloan", async () => {
 
     aaveV2handler = await (
       await ethers.getContractFactory("AaveV2Handler")
-    ).deploy(WRAPPED_NATIVE_TOKEN, AAVEPROTOCOL_V2_PROVIDER, callbackHandler.address);
-
+    ).deploy(
+      WRAPPED_NATIVE_TOKEN,
+      AAVEPROTOCOL_V2_PROVIDER,
+      callbackHandler.address
+    );
 
     strategyModule = await getStrategyModule(
       alice.address,
@@ -148,33 +146,19 @@ describe("AaveV2 flashloan", async () => {
   });
 
   describe("ٔNormal", function () {
-    beforeEach(async function () {
-      // const { userSA } = await setupTests();
-      // const depositAmount = ethers.utils.parseEther("10000");
-
-      // await token
-      //   .connect(providerAddress)
-      //   .approve(lendingPool.address, depositAmount);
-
-      // await lendingPool.connect(providerAddress).deposit(
-      //   token.address,
-      //   depositAmount,
-      //   userSA.address,
-      //   0,
-      // );
-    });
-
     it("single asset with no debt", async function () {
       const { userSA, ecdsaModule, errAbi } = await setupTests();
-      const value = ethers.utils.parseEther("10000");
+      const value = ethers.utils.parseEther("1");
       const handler = aaveV2handler.address;
 
       const data = (
         await ethers.getContractFactory("AaveV2Handler")
       ).interface.encodeFunctionData(
         "flashLoan(address[],uint256[],uint256[],bytes)",
-        [[token.address], [value], [AAVE_RATEMODE.NODEBT], '0x']
+        [[token.address], [value], [AAVE_RATEMODE.NODEBT], "0x"]
       );
+
+      const loanFee = value.mul(9).div(10000);
 
       await token.connect(providerAddress).transfer(userSA.address, value);
 
@@ -189,7 +173,7 @@ describe("AaveV2 flashloan", async () => {
           0
         );
 
-      const beforeExecBalance = await token.balanceOf(userSA.address)
+      const beforeExecBalance = await token.balanceOf(userSA.address);
 
       try {
         await strategyModule.requiredTxFee(userSA.address, transaction);
@@ -207,52 +191,132 @@ describe("AaveV2 flashloan", async () => {
 
       const afterExecBalance = await token.balanceOf(userSA.address);
 
-      console.log(userSA.address, beforeExecBalance, afterExecBalance)
+      expect(beforeExecBalance.sub(afterExecBalance)).to.be.eq(loanFee);
 
-      // expect(beforeExecBalance.sub(afterExecBalance)).to.be.eq(value);
+      expect(await token.balanceOf(strategyModule.address)).to.be.eq(0);
+    });
 
-      // expect(await AWrappedETH.balanceOf(userSA.address)).to.be.eq(value);
+    it("single asset with variable rate by borrowing from itself", async function () {
+      const { userSA, ecdsaModule, errAbi } = await setupTests();
 
-      // expect(await AWrappedETH.balanceOf(strategyModule.address)).to.be.eq(0);
+      const depositAmount = ethers.utils.parseEther("10000");
 
-      // expect(await waffle.provider.getBalance(strategyModule.address)).to.be.eq(
-      //   0
-      // );
+      await WrappedETH.connect(wethProviderAddress).approve(
+        lendingPool.address,
+        depositAmount
+      );
+
+      await lendingPool
+        .connect(wethProviderAddress)
+        .deposit(WrappedETH.address, depositAmount, userSA.address, 0);
+
+      const value = ethers.utils.parseEther("1");
+      const handler = aaveV2handler.address;
+
+      const data = (
+        await ethers.getContractFactory("AaveV2Handler")
+      ).interface.encodeFunctionData(
+        "flashLoan(address[],uint256[],uint256[],bytes)",
+        [[WrappedETH.address], [value], [AAVE_RATEMODE.VARIABLE], "0x"]
+      );
+
+      const { transaction, signature } =
+        await buildEcdsaModuleAuthorizedStrategyTx(
+          handler,
+          data,
+          userSA,
+          smartAccountOwner,
+          ecdsaModule.address,
+          strategyModule,
+          0
+        );
+
+      const beforeExecBalance = await WrappedETH.balanceOf(userSA.address);
+
+      try {
+        await strategyModule.requiredTxFee(userSA.address, transaction);
+      } catch (error) {
+        fee = decodeError(error, errAbi).args;
+        fee = fee[0];
+      }
+
+      await strategyModule.execStrategy(
+        userSA.address,
+        transaction,
+        signature,
+        { value: fee }
+      );
+
+      const afterExecBalance = await WrappedETH.balanceOf(userSA.address);
+
+      expect(afterExecBalance.sub(beforeExecBalance)).to.be.eq(value);
+
+      expect(await variableDebtWETH.balanceOf(userSA.address)).to.be.eq(value);
+
+      expect(await WrappedETH.balanceOf(strategyModule.address)).to.be.eq(0);
+    });
+
+    it("multiple assets with no debt", async function () {
+      const { userSA, ecdsaModule, errAbi } = await setupTests();
+
+      const value = ethers.utils.parseEther("1");
+      const handler = aaveV2handler.address;
+
+      const data = (
+        await ethers.getContractFactory("AaveV2Handler")
+      ).interface.encodeFunctionData(
+        "flashLoan(address[],uint256[],uint256[],bytes)",
+        [
+          [token.address, WrappedETH.address],
+          [value, value],
+          [AAVE_RATEMODE.NODEBT, AAVE_RATEMODE.NODEBT],
+          "0x",
+        ]
+      );
+
+      const loanFee = value.mul(9).div(10000);
+
+      await token.connect(providerAddress).transfer(userSA.address, value);
+      await WrappedETH.connect(wethProviderAddress).transfer(
+        userSA.address,
+        value
+      );
+
+      const { transaction, signature } =
+        await buildEcdsaModuleAuthorizedStrategyTx(
+          handler,
+          data,
+          userSA,
+          smartAccountOwner,
+          ecdsaModule.address,
+          strategyModule,
+          0
+        );
+
+      const beforeExecBalance = await token.balanceOf(userSA.address);
+      const beforeExecBalanceWeth = await WrappedETH.balanceOf(userSA.address);
+
+      try {
+        await strategyModule.requiredTxFee(userSA.address, transaction);
+      } catch (error) {
+        fee = decodeError(error, errAbi).args;
+        fee = fee[0];
+      }
+
+      await strategyModule.execStrategy(
+        userSA.address,
+        transaction,
+        signature,
+        { value: fee }
+      );
+
+      const afterExecBalance = await token.balanceOf(userSA.address);
+      const afterExecBalanceWeth = await WrappedETH.balanceOf(userSA.address);
+
+      expect(beforeExecBalance.sub(afterExecBalance)).to.be.eq(loanFee);
+      expect(beforeExecBalanceWeth.sub(afterExecBalanceWeth)).to.be.eq(loanFee);
+
+      expect(await token.balanceOf(strategyModule.address)).to.be.eq(0);
     });
   });
 });
-
-// function _getFlashloanParams(tos, configs, faucets, tokens, amounts) {
-//   const data = [
-//     "0x" +
-//       abi
-//         .simpleEncode(
-//           "drainTokens(address[],address[],uint256[])",
-//           faucets,
-//           tokens,
-//           amounts
-//         )
-//         .toString("hex"),
-//   ];
-
-//   const params = web3.eth.abi.encodeParameters(
-//     ["address[]", "bytes32[]", "bytes[]"],
-//     [tos, configs, data]
-//   );
-//   return params;
-// }
-
-// function _getFlashloanCubeData(assets, amounts, modes, params) {
-//   const data = abi.simpleEncode(
-//     "flashLoan(address[],uint256[],uint256[],bytes)",
-//     assets,
-//     amounts,
-//     modes,
-//     util.toBuffer(params)
-//   );
-//   return data;
-// }
-
-// function _getFlashloanFee(value) {
-//   return value.mul(new BN("9")).div(new BN("10000"));
-// }
