e5deadb8354095ffc452c9243626ff64eba479ab
zakrad <daniyal.rad@gmail.com>
2023-11-01 23:24:18 +0330

    🤡 add mock handler

---
+++ defi-strategies/contracts/mocks/MockHandler.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {IStrategyModule} from "contracts/interfaces/IStrategyModule.sol";
+import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+
+/**
+ * @title Mock handler
+ */
+
+contract MockHandler {
+    using ERC165Checker for address;
+
+    IStrategyModule public strategyModule;
+
+    bytes4 private constant _STRATEGY_INTERFACE_ID =
+        type(IStrategyModule).interfaceId;
+
+    function reEnter(address strategyModule) external {
+        IStrategyModule.StrategyTransaction memory _tx = IStrategyModule
+            .StrategyTransaction(0, 0, "0x0");
+        IStrategyModule(strategyModule).execStrategy(
+            strategyModule,
+            _tx,
+            "0x0"
+        );
+    }
+
+    function checkInterface(
+        address strategyModule
+    ) external view returns (bool) {
+        return strategyModule.supportsInterface(_STRATEGY_INTERFACE_ID);
+    }
+}
